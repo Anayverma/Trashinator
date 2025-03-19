a2376b64305d939a215301279856258a7db7a16c
Anayverma <anayverma2147@gmail.com>
2025-03-19 19:47:06 +0530

    error cor

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def model_request():
         return jsonify({'error': f'An error occurred: {str(e)}'})
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
